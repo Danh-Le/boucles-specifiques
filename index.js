d681fa1e168f052247a25f7b0a5c0668f2b968de
Danh-Le <quang.danh.le@gmail.com>
2022-06-28 13:43:38 +0200

    Ex1 done

---
+++ index.js
@@ -0,0 +1,20 @@
+// 01 - Map Double
+const array = [1, 2, 3, 4, 5]
+const double = array.map((array) => {
+    return array *2
+})
+console.log(double)
+
+// 02 - Map Names
+
+// 03 - Filter Numbers
+
+// 04 - Filter Even
+
+// 05 - Cake
+
+// 06 - Cake v2
+
+// 07 - Factorielle forEach
+
+// 08 - Chess board
